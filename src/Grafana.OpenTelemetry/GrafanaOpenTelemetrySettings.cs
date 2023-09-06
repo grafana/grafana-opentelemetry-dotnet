a84e74fc441d001fb3d76d6a23632735680c7195
Johannes Tax <johannes.tax@grafana.com>
2023-09-06 21:24:39 +0200

    Improve exporter setup (#3)
    * Make sure the cloud exporter is used when env vars are set
    
    * Add documentation

--- src/Grafana.OpenTelemetry/GrafanaOpenTelemetrySettings.cs
+++ src/Grafana.OpenTelemetry/GrafanaOpenTelemetrySettings.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Grafana.OpenTelemetry
 {
     /// <summary>
@@ -8,11 +10,29 @@ namespace Grafana.OpenTelemetry
         /// <summary>
         /// Gets or sets the exporter settings for sending telemetry data to Grafana.
         ///
-        /// By default, data is sent locally to the default OTLP port.
+        /// By default, this is set to:
+        ///  1. j <see cref="CloudOtlpExporter"/> initialized by environment variables, or if
+        ///     the environment variables aren't present, it is set to
+        ///  2. a <see cref="AgentOtlpExporter"/> with the default OTLP endpoint.
         ///
         /// If set to `null`, no exporter will be initialized by the OpenTelemetry .NET
         /// distribution for Grafana.
         /// </summary>
-        public ExporterSettings ExporterSettings { get; set; } = new AgentOtlpExporter();
+        public ExporterSettings ExporterSettings { get; set; }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="GrafanaOpenTelemetrySettings"/>.
+        /// </summary>
+        public GrafanaOpenTelemetrySettings()
+        {
+            try
+            {
+                ExporterSettings = new CloudOtlpExporter();
+            }
+            catch (Exception)
+            {
+                ExporterSettings = new AgentOtlpExporter();
+            }
+        }
     }
 }
