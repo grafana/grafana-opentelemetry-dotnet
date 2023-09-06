810c4923202baa6e57513bd4789a71ce75bc55dc
Matthew Hensley <matthew.hensley@grafana.com>
2023-09-06 15:40:38 -0400

    basic check if CloudOtlpExporter respects env vars

---
+++ tests/Grafana.OpenTelemetry.Tests/CloudOtlpExporterTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Grafana.OpenTelemetry.Tests;
+
+public class CloudOtlpExporterTest
+{
+    [Fact]
+    public void CloudOtlpExporterEnvVars()
+    {
+        Environment.SetEnvironmentVariable(CloudOtlpExporter.ApiKeyEnvVarName, "a_secret");
+        Environment.SetEnvironmentVariable(CloudOtlpExporter.InstanceIdEnvVarName, "12345");
+        Environment.SetEnvironmentVariable(CloudOtlpExporter.ZoneEnvVarName, "test-example-0");
+        var settings = new CloudOtlpExporter();
+
+        Assert.Equal("a_secret", settings.ApiKey);
+        Assert.Equal("12345", settings.InstanceId);
+        Assert.Equal("test-example-0", settings.Zone);
+    }
+}
