cff18d470e31a3e229c60cdf9d2601e273fba921
Johannes Tax <johannes.tax@grafana.com>
2023-09-26 13:25:58 +0200

    De-activate AWSLambda instrumentation by default (#18)
    * De-activate AWSLambda instrumentation by default
    
    * Linter issue

--- tests/Grafana.OpenTelemetry.Tests/GrafanaOpenTelemetrySettingsTest.cs
+++ tests/Grafana.OpenTelemetry.Tests/GrafanaOpenTelemetrySettingsTest.cs
@@ -12,12 +12,26 @@ namespace Grafana.OpenTelemetry.Tests
             Environment.SetEnvironmentVariable(GrafanaOpenTelemetrySettings.DisableInstrumentationsEnvVarName, null);
         }
 
-        [Fact]
+        [Fact(Skip = "provider builders crashes on enabling AWSLambda instrumentation by default")]
         public void DefaultInstrumentations()
         {
             var settings = new GrafanaOpenTelemetrySettings();
 
             Assert.Equal(new HashSet<Instrumentation>((Instrumentation[])Enum.GetValues(typeof(Instrumentation))), settings.Instrumentations);
+        }
+
+        [Fact]
+        public void DefaultInstrumentationsWithoutAWSLambda()
+        {
+            var settings = new GrafanaOpenTelemetrySettings();
+
+            // De-activate AWSLambda instrumenation until this issue is resolved: https://github.com/grafana/app-o11y/issues/378
+            //
+            // Once it's resolved, this test can be removed and the `DefaultInstrumentation` test can be re-activated.
+            var expectedSettings = new HashSet<Instrumentation>((Instrumentation[])Enum.GetValues(typeof(Instrumentation)));
+            expectedSettings.Remove(Instrumentation.AWSLambda);
+
+            Assert.Equal(expectedSettings, settings.Instrumentations);
         }
 
         [Fact]
@@ -27,10 +41,8 @@ namespace Grafana.OpenTelemetry.Tests
 
             var settings = new GrafanaOpenTelemetrySettings();
 
-            var expected = new HashSet<Instrumentation>((Instrumentation[])Enum.GetValues(typeof(Instrumentation)));
-            expected.ExceptWith(new Instrumentation[] { Instrumentation.Process, Instrumentation.NetRuntime });
-
-            Assert.Equal(expected, settings.Instrumentations);
+            Assert.False(settings.Instrumentations.Contains(Instrumentation.Process));
+            Assert.False(settings.Instrumentations.Contains(Instrumentation.NetRuntime));
         }
 
         [Fact]
@@ -40,10 +52,8 @@ namespace Grafana.OpenTelemetry.Tests
 
             var settings = new GrafanaOpenTelemetrySettings();
 
-            var expected = new HashSet<Instrumentation>((Instrumentation[])Enum.GetValues(typeof(Instrumentation)));
-            expected.ExceptWith(new Instrumentation[] { Instrumentation.Process, Instrumentation.NetRuntime });
-
-            Assert.Equal(expected, settings.Instrumentations);
+            Assert.False(settings.Instrumentations.Contains(Instrumentation.Process));
+            Assert.False(settings.Instrumentations.Contains(Instrumentation.NetRuntime));
         }
     }
 }
