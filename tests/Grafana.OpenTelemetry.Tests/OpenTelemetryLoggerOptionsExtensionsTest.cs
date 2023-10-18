56734bf0f64561247f889ec2c1b26363810d6d59
Johannes Tax <johannes.tax@grafana.com>
2023-10-18 16:25:13 +0200

    Add OTel logging support

---
+++ tests/Grafana.OpenTelemetry.Tests/OpenTelemetryLoggerOptionsExtensionsTest.cs
@@ -0,0 +1,24 @@
+using Grafana.OpenTelemetry;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using Xunit;
+
+namespace Grafana.OpenTelemetry.Tests
+{
+    public class OpenTelemetryLoggerOptionsExtensionsTest
+    {
+        [Fact]
+        public void BuildDefault()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(logging =>
+                {
+                    logging.UseGrafana();
+                });
+            });
+
+            var logger = loggerFactory.CreateLogger<OpenTelemetryLoggerOptionsExtensionsTest>();
+        }
+    }
+}
