67b74ea4f7d61a426f26df5b6cc894485d354c9a
Johannes Tax <johannes.tax@grafana.com>
2023-11-06 17:54:42 +0100

    Format issues

--- src/Grafana.OpenTelemetry.Base/GrafanaOpenTelemetrySettings.cs
+++ src/Grafana.OpenTelemetry.Base/GrafanaOpenTelemetrySettings.cs
@@ -127,7 +127,7 @@ namespace Grafana.OpenTelemetry
                 {
                     DeploymentEnvironment = deploymentEnvironment.ToLower();
                 }
-	    }
+            }
         }
     }
 }
