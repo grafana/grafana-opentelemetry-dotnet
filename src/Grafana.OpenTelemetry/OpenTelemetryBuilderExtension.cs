62169d07a7f514ca98f9af7a0ed05ad3e8f69b2e
Johannes Tax <johannes.tax@grafana.com>
2023-11-07 16:29:32 +0100

    Add `UseGrafana` extension method for ASP.NET Core projects (#32)

---
+++ src/Grafana.OpenTelemetry/OpenTelemetryBuilderExtension.cs
@@ -0,0 +1,35 @@
+//
+// Copyright Grafana Labs
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#if !NETFRAMEWORK
+
+using System;
+using OpenTelemetry;
+
+namespace Grafana.OpenTelemetry
+{
+    /// <summary>
+    /// Extension for the <see cref="OpenTelemetry.OpenTelemetryBuilder"/> provided by the OpenTelemetry .NET distribution 
+    /// for Grafana.
+    ///
+    /// This is used for easier configuration for ASP.NET Core projects.
+    /// </summary>
+    public static class OpenTelemetryBuilderExtension
+    {
+        /// <summary>
+        /// Sets up tracing and metrics with the OpenTelemetry .NET distribution for Grafana.
+        /// </summary>
+        /// <param name="builder">A <see cref="OpenTelemetryBuilder"/></param>
+        /// <param name="configure">A callback for customizing default Grafana OpenTelemetry settings</param>
+        /// <returns>A modified <see cref="OpenTelemetryBuilder"/> </returns>
+        public static OpenTelemetryBuilder UseGrafana(this OpenTelemetryBuilder builder, Action<GrafanaOpenTelemetrySettings> configure = default)
+        {
+            return builder
+                .WithTracing(tracerProviderBuilder => tracerProviderBuilder.UseGrafana(configure))
+                .WithMetrics(metricProviderBuilder => metricProviderBuilder.UseGrafana(configure));
+        }
+    }
+}
+#endif
