56202f85207a4f146964f9118cee266ee5e42896
Johannes Tax <johannes.tax@grafana.com>
2023-11-08 14:54:10 +0100

    Default to using stable HTTP semantic conventions

--- src/Grafana.OpenTelemetry.Base/MeterProviderBuilderExtensions.cs
+++ src/Grafana.OpenTelemetry.Base/MeterProviderBuilderExtensions.cs
@@ -31,6 +31,12 @@ namespace Grafana.OpenTelemetry
 
             GrafanaOpenTelemetryEventSource.Log.InitializeDistribution(settings);
 
+	    // Default to using stable HTTP semantic conventions
+            if (Environment.GetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN") == null)
+	    {
+                Environment.SetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN", "http");
+	    }
+
             return builder
                 .AddGrafanaExporter(settings?.ExporterSettings)
                 .AddInstrumentations(settings?.Instrumentations)
