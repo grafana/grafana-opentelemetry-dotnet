097da2cdb014e93233bacf56a57e5c972fadcacf
Matt Hensley <130569+matt-hensley@users.noreply.github.com>
2023-11-09 09:07:28 -0500

    Updates reflection lookup / OWIN tracing instrumentation (#45)

--- src/Grafana.OpenTelemetry.Base/ReflectionHelper.cs
+++ src/Grafana.OpenTelemetry.Base/ReflectionHelper.cs
@@ -15,7 +15,7 @@ namespace Grafana.OpenTelemetry
         {
             var assembly = Assembly.Load(assemblyName);
             var type = assembly.GetType(typeName);
-            var method = type.GetMethod(methodName, arguments.Select(obj => obj.GetType()).ToArray());
+            var method = type.GetMethod(methodName, arguments.Select(obj => obj is null ? null : obj.GetType()).ToArray());
             method.Invoke(null, arguments);
         }
     }
