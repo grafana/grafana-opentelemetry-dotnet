703779abf131247c5c165fda65fc2893cbe22332
martincostello <martin@martincostello.com>
2025-06-02 14:57:46 +0100

    Use latest OATS
    Use the latest stable release of OATS.

--- scripts/run-oats-tests.ps1
+++ scripts/run-oats-tests.ps1
@@ -5,7 +5,7 @@ $InformationPreference = "Continue"
 $ProgressPreference = "SilentlyContinue"
 
 # TODO Pin to specific version and then update with renovate
-${env:OATS_VERSION}="173ac1f6a190c2ffd156a98dfe923c70a2a0c3ca"
+${env:OATS_VERSION}="latest"
 
 go install "github.com/grafana/oats@${env:OATS_VERSION}"
 & "${env:GOPATH}/bin/oats" --timeout=5m ./docker/docker-compose-aspnetcore
