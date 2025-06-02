6cb76a8282a38b512a7d9444e42332975e44d941
martincostello <martin@martincostello.com>
2025-06-02 11:56:03 +0100

    Fix timeout
    Fix timeout no longer being consumed from `TESTCASE_TIMEOUT` env var and instead use `--timeout` flag.

--- scripts/run-oats-tests.ps1
+++ scripts/run-oats-tests.ps1
@@ -7,9 +7,5 @@ $ProgressPreference = "SilentlyContinue"
 # TODO Pin to specific version and then update with renovate
 ${env:OATS_VERSION}="173ac1f6a190c2ffd156a98dfe923c70a2a0c3ca"
 
-${env:TESTCASE_SKIP_BUILD}="true"
-${env:TESTCASE_TIMEOUT}="5m"
-${env:TESTCASE_BASE_PATH}="./docker"
-
 go install "github.com/grafana/oats@${env:OATS_VERSION}"
-& "${env:GOPATH}/bin/oats" ./docker/docker-compose-aspnetcore
+& "${env:GOPATH}/bin/oats" --timeout=5m ./docker/docker-compose-aspnetcore
--- scripts/run-oats-tests.sh
+++ scripts/run-oats-tests.sh
@@ -5,9 +5,5 @@ set -euo pipefail
 # TODO Pin to specific version and then update with renovate
 export OATS_VERSION=latest
 
-export TESTCASE_SKIP_BUILD=true
-export TESTCASE_TIMEOUT=5m
-export TESTCASE_BASE_PATH=./docker
-
 go install "github.com/grafana/oats@${OATS_VERSION}"
-${GOPATH}/bin/oats ./docker/docker-compose-aspnetcore
+${GOPATH}/bin/oats --timeout=5m ./docker/docker-compose-aspnetcore
