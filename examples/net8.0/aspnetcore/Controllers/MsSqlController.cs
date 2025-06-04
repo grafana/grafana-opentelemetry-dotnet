10ad08ee23e421d0f7169c05dd756d04837500d1
Martin Costello <martin@martincostello.com>
2025-06-04 14:12:44 +0100

    Refactor example app (#139)
    * Update .gitignore
    
    - Ignore `build` from docker compose.
    - Remove redundant lines.
    
    * Add recommended VS Code extensions
    
    Add Visual Studio Code configuration file for recommended extensions.
    
    * Refactor example app
    
    - Update NuGet packages to latest versions.
    - Add Grafana logging.
    - Remove redundant configuration file.
    - Add HttpClientFactory.
    - Use primary constructors.
    - Use async methods where available.
    - Use simple using statements.
    - Use structured logging.
    - Use collection expressions.
    - Remove unused fields.

--- examples/net8.0/aspnetcore/Controllers/MsSqlController.cs
+++ examples/net8.0/aspnetcore/Controllers/MsSqlController.cs
@@ -8,61 +8,53 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
 namespace aspnetcore.Controllers;
+
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class MsSqlController : ControllerBase
+public class MsSqlController(SqlConnection db) : ControllerBase
 {
-    private readonly ILogger<MsSqlController> _logger;
-    private readonly SqlConnection _db;
-
-    public MsSqlController(ILogger<MsSqlController> logger, SqlConnection db)
-    {
-        _logger = logger;
-        _db = db;
-    }
-
     [HttpGet]
     public async Task<ActionResult<IEnumerable<string>>> Tables()
     {
-        this._db.Open();
+        await db.OpenAsync();
 
-        using (var command = _db.CreateCommand())
+        await using var command = db.CreateCommand();
+
+        command.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
+
+        var tables = new List<string>();
+
+        await using (var reader = await command.ExecuteReaderAsync())
         {
-            command.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
-            var tables = new List<string>();
-
-            using (var reader = await command.ExecuteReaderAsync())
+            while (await reader.ReadAsync())
             {
-                while (await reader.ReadAsync())
-                {
-                    tables.Add(reader.GetString(0));
-                }
+                tables.Add(reader.GetString(0));
             }
-            return Ok(tables);
         }
+
+        return Ok(tables);
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<string>>> ServerInfo()
     {
-        this._db.Open();
+        await db.OpenAsync();
 
-        using (var command = _db.CreateCommand())
+        await using var command = db.CreateCommand();
+
+        command.CommandText = "sp_server_info";
+        command.CommandType = CommandType.StoredProcedure;
+
+        var serverInfo = new List<string>();
+
+        await using (var reader = await command.ExecuteReaderAsync())
         {
-            command.CommandText = "sp_server_info";
-            command.CommandType = CommandType.StoredProcedure;
-
-            var serverInfo = new List<string>();
-
-            using (var reader = await command.ExecuteReaderAsync())
+            while (await reader.ReadAsync())
             {
-                while (await reader.ReadAsync())
-                {
-                    serverInfo.Add($"ID={reader.GetInt32(0)} , NAME={reader.GetString(1)} , VALUE={reader.GetString(2)}");
-                }
+                serverInfo.Add($"ID={reader.GetInt32(0)} , NAME={reader.GetString(1)} , VALUE={reader.GetString(2)}");
             }
-
-            return Ok(serverInfo);
         }
+
+        return Ok(serverInfo);
     }
 }
--- examples/net8.0/aspnetcore/Controllers/RedisController.cs
+++ examples/net8.0/aspnetcore/Controllers/RedisController.cs
@@ -8,22 +8,14 @@ using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 
 namespace aspnetcore.Controllers;
+
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class RedisController : ControllerBase
+public class RedisController(IDatabase database, ILogger<RedisController> logger) : ControllerBase
 {
     const string LIST_KEY = "list";
-    private readonly ILogger<RedisController> _logger;
-    private IDatabase _redisDb;
-
-    public RedisController(ILogger<RedisController> logger, IDatabase database)
-    {
-        _logger = logger;
-        _redisDb = database;
-    }
 
     [HttpGet]
-    // public async Task<ActionResult<string>> LeftPush([FromBody] LeftPushBody data)
     public async Task<ActionResult<string>> LeftPush()
     {
         if (!ModelState.IsValid)
@@ -32,16 +24,20 @@ public class RedisController : ControllerBase
         }
 
         var data = new LeftPushBody { Name = "test" };
-        var length = await _redisDb.ListLeftPushAsync(LIST_KEY, data.Name);
-        _logger.LogInformation($"LeftPush: {data.Name} - {length}");
+        var length = await database.ListLeftPushAsync(LIST_KEY, data.Name);
+
+        logger.LogInformation("LeftPush: {Name} - {Length}", data.Name, length);
+
         return Ok();
     }
 
     [HttpGet]
     public async Task<ActionResult<string>> LeftPop()
     {
-        var value = await _redisDb.ListLeftPopAsync(LIST_KEY);
-        _logger.LogInformation($"LeftPop: {value}");
+        var value = await database.ListLeftPopAsync(LIST_KEY);
+
+        logger.LogInformation("LeftPop: {Value}", value);
+
         return Ok(value);
     }
 
