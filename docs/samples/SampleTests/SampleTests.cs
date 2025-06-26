1b5e3dd01953bf3a3c8806f4f26e13d77f403437
martincostello <martin@martincostello.com>
2025-06-26 15:06:27 +0100

    Assert logs
    Assert OTel logs.

--- docs/samples/SampleTests/SampleTests.cs
+++ docs/samples/SampleTests/SampleTests.cs
@@ -1,9 +1,15 @@
+using System.Diagnostics;
 using System.Net.Http.Json;
 using Grafana.OpenTelemetry.Sample;
 using MartinCostello.Logging.XUnit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
 
 namespace SampleTests;
 
@@ -34,15 +40,43 @@ public class SampleTests(ITestOutputHelper outputHelper)
                 item.Summary,
                 new string[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" });
         });
+
+        Assert.NotEmpty(fixture.Logs);
+        //Assert.NotEmpty(fixture.Metrics); // TODO Why is this empty?
+        //Assert.NotEmpty(fixture.Traces);
     }
 
     public class OpenTelemetryApplication(ITestOutputHelper outputHelper) : WebApplicationFactory<WeatherForecast>, ITestOutputHelperAccessor
     {
         public ITestOutputHelper? OutputHelper { get; set; } = outputHelper;
 
+        public IList<LogRecord> Logs { get; } = [];
+
+        public IList<Metric> Metrics { get; } = [];
+
+        public IList<Activity> Traces { get; } = [];
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureAppConfiguration((configBuilder) =>
+            {
+                var config = new[]
+                {
+                    KeyValuePair.Create<string, string?>("OTEL_METRIC_EXPORT_INTERVAL", "00:00:01"),
+                };
+
+                configBuilder.AddInMemoryCollection(config);
+            });
+
             builder.ConfigureLogging(logging => logging.ClearProviders().AddXUnit(this));
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddOpenTelemetry()
+                        .WithLogging((builder) => builder.AddInMemoryExporter(Logs))
+                        .WithMetrics((builder) => builder.AddInMemoryExporter(Metrics))
+                        /*.WithTracing((builder) => builder.AddInMemoryExporter(Traces))*/;
+            });
         }
     }
 }
