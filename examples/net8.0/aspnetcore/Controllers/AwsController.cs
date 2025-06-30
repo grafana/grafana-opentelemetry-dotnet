59fef3b3349083de6d825d3c5e3dcb228597f5ea
Martin Costello <martin@martincostello.com>
2025-06-30 18:23:20 +0100

    Verify AWS instrumentation in OATS (#172)
    Extend the OATS tests to assert on AWS SDK traces using LocalStack.

---
+++ examples/net8.0/aspnetcore/Controllers/AwsController.cs
@@ -0,0 +1,26 @@
+//
+// Copyright Grafana Labs
+// SPDX-License-Identifier: Apache-2.0
+//
+
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AwsController(IAmazonS3 client, ILogger<AwsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<string[]>> ListBuckets()
+    {
+        var response = await client.ListBucketsAsync();
+
+        var buckets = response.Buckets.Select(o => o.BucketName).ToArray();
+
+        logger.LogInformation("Found {Count} buckets.", buckets.Length);
+
+        return this.Ok(buckets);
+    }
+}
