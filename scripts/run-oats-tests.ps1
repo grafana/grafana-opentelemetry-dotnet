f82a7d1b75cb59198f3271ab4ff18eb0ec229ea0
Martin Costello <martin@martincostello.com>
2025-07-22 15:50:13 +0100

    Speed up OATS acceptance tests (#198)
    * Reduce OTLP export period
    
    Emit OTLP signals more frequently to speed-up OATS acceptance tests.
    
    * Bump OATS in scripts
    
    Update OATS to v0.4.0 in the local development scripts.

--- scripts/run-oats-tests.ps1
+++ scripts/run-oats-tests.ps1
@@ -5,7 +5,7 @@ $InformationPreference = "Continue"
 $ProgressPreference = "SilentlyContinue"
 
 # renovate: datasource=github-releases depName=oats packageName=grafana/oats
-${env:OATS_VERSION}="v0.3.2"
+${env:OATS_VERSION}="v0.4.0"
 
 go install "github.com/grafana/oats@${env:OATS_VERSION}"
 & "${env:GOPATH}/bin/oats" --timeout=5m ./docker/docker-compose-aspnetcore
--- scripts/run-oats-tests.sh
+++ scripts/run-oats-tests.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # renovate: datasource=github-releases depName=oats packageName=grafana/oats
-export OATS_VERSION=v0.3.2
+export OATS_VERSION=v0.4.0
 
 go install "github.com/grafana/oats@${OATS_VERSION}"
 ${GOPATH}/bin/oats --timeout=5m ./docker/docker-compose-aspnetcore
